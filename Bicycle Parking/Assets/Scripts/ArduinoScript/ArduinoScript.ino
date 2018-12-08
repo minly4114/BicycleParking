451660cbb943479b7fae88d7986059000fb6a6d5
Vladislav Vasilenko <xfvladikxf@gmail.com>
2018-12-08 15:48:19 +0300

    Добавил распознавание карт + размещение по парковке

---
+++ Bicycle Parking/Assets/Scripts/ArduinoScript/ArduinoScript.ino
@@ -0,0 +1,82 @@
+#include <MFRC522.h>
+#include <SPI.h>
+
+#define GREEN_LED 7
+#define RED_LED 6
+
+#define RST_PIN 9
+#define SS_PIN 10 
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+byte nuidPICC[4];
+
+void setup() {
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  Serial.begin(9600);
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+}
+
+void loop() {  
+  // Look for new cards
+  if ( ! rfid.PICC_IsNewCardPresent())
+  {
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+    return;
+  }
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+  {
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, HIGH);
+    delay(1000);
+    return;
+  }
+  
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+  }
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+
+  digitalWrite(GREEN_LED, HIGH);
+  delay(250);
+}
+
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
+void printHex(byte *buffer, byte bufferSize) {  
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? "0" : "");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
+
